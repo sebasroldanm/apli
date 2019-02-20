6f6024151fc30cfe22a4031407d009023ce47771
Sebastian Roldan <sebasroldanm@gmail.com>
2019-02-20 10:26:06 -0500

    Intento Roles con Seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new \App\Role();
+		$admin->name = 'admin';
+		$admin->display_name = 'User Administrator'; // optional
+		$admin->description = 'User is allowed to manage all resources'; // optional
+		$admin->save();
+
+		$owner = new \App\Role();
+		$owner->name = 'owner';
+		$owner->display_name = 'Owner'; // optional
+		$owner->description = 'User is allowed to manage everything resources'; // optional
+		$owner->save();
+
+		//AQUI SE DECLARA E RESTO
+
+
+		$adminUser = \App\User::create([
+		'name' => 'admin',
+		'email' => 'admin@mail.com',
+		'password' => \Illuminate\Support\Facades\Hash::make('secret')
+		]);
+
+		$adminUser->save();
+
+		$ownerUser = \App\User::create([
+		'name' => 'owner',
+		'email' => 'owner@mail.com',
+		'password' => \Illuminate\Support\Facades\Hash::make('secret')
+		]);
+    }
+}
